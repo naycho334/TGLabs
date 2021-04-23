d15a6c5062aac61799033392b801bd6f7c87b5c9
Samir El Khaouti <elkhaouti@gmail.com>
2021-04-23 18:10:12 +0000

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+exec mkdir /home/tglabs/app/test
+# sudo apt -y install nodejs
+# sudo apt -y install npm
+
+# cd /home/tglabs/app
+# npm run build
+
+# cp /home/tglabs/app/build /home/tglabs/public_html/
